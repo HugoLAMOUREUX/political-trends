31cc6902cc060cfc467f1a0be970a0d4b35ba879
HugoLAMOUREUX <hugo.lamoureux18@gmail.com>
2026-01-02 15:55:28 +0100

    fix: cors deployed

--- api/src/index.js
+++ api/src/index.js
@@ -17,7 +17,12 @@ if (ENVIRONMENT === "development") {
 
 require("./services/mongo");
 
-app.use(cors({ credentials: true, origin: [APP_URL, ADMIN_URL, "https://political-trends.onrender.com/"] }));
+app.use(
+  cors({
+    credentials: true,
+    origin: [APP_URL, ADMIN_URL, "https://political-trends.onrender.com/", "https://hugolamoureux.github.io"],
+  }),
+);
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
